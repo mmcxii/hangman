3a5a58aad49608e68dd8b5370f034aae8628a899
Nich Secord <nicholassecord@gmail.com>
2019-06-01 13:21:10 -0700

    refactored and improved structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,96 @@
-// Possible Answers
+// Section 0: Variables //
+
+let current;
+let answer;
+let hint;
+let winCounter;
+let numGuesses;
+let usedLetters;
+
 const answers = [
     {
         name: 'bourbon',
+        hint: 'Sweet Kentucky Whiskey',
     },
     {
         name: 'rye',
+        hint: 'Spicy single gain whiskey',
     },
     {
         name: 'scotch',
+        hint: 'Neat or on the rocks?',
+    },
+    {
+        name: 'cabernet',
+        hint: 'Sauv or Franc?',
+    },
+    {
+        name: 'merlot',
+        hint: "I'm not drinking f**king ____!",
+    },
+    {
+        name: 'gin',
+        hint: 'Mixes well with tonic and lime',
+    },
+    {
+        name: 'sangiovese',
+        hint: 'Fruit forward Italian red',
+    },
+    {
+        name: 'tequila',
+        hint: 'Spring break!',
     },
 ];
 
-// Linking to Document
 const currentWordField = document.getElementById('current-word-field');
 const answerField = document.getElementById('answer-field');
-let maskedAnswer = '';
-let answer = answers[r(0, answers.length)].name;
-
 const winCounterField = document.getElementById('win-counter-field');
-let winCounter = 0;
-
 const numGuessesField = document.getElementById('num-guesses-field');
-let numGuesses = 15;
-
 const usedLettersField = document.getElementById('used-letters-field');
-let usedLetters = [];
 
-// Initialize Document
+// Section 1: Initialize Document //
+
+current = answers[r(0, answers.length)];
+answer = current.name;
+hint = current.hint;
+maskAnswer();
+winCounter = 0;
+numGuesses = 15;
+usedLetters = [];
 numGuessesField.innerHTML = numGuesses;
 winCounterField.innerHTML = winCounter;
 
-//// Creates a String of '_' for each letter of answer
-for (let i = 0; i < answer.length; i++) {
-    maskedAnswer += '_ ';
+// Section X: Functions //
+
+// Listen for Keyboard events
+let addFunctionHere;
+
+// Create Masked Answer
+function maskAnswer() {
+    maskedAnswer = document.createElement('ul');
+
+    for (let i = 0; i < answer.length; i++) {
+        maskedAnswer.setAttribute('id', 'current');
+        letter = document.createElement('li');
+        letter.setAttribute('class', 'letter');
+        if (answer[i] === ' ') {
+            maskedAnswer.innerHTML = ' ';
+        } else {
+            letter.innerHTML = '_';
+        }
+
+        currentWordField.appendChild(maskedAnswer);
+        maskedAnswer.appendChild(letter);
+    }
 }
-currentWordField.innerHTML = maskedAnswer;
 
 // Generates Random Number
-// Source MDN
+//// Source MDN
 function r(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+console.log(answer);
+console.log(hint);

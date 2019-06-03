2e7b52f61b6eb8d58c84cc22a51b6963b2af46db
Nich Secord <nicholassecord@gmail.com>
2019-06-03 11:03:53 -0700

    completed app. finished styling, added buttons, linked all functionality, structured and organized app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ const answers = [
     },
     {
         name: 'merlot',
-        hint: "I'm not drinking f**king ____!",
+        hint: "I'm not drinking f**king ______!",
     },
     {
         name: 'gin',
@@ -46,68 +46,119 @@ const answers = [
 const answerField = document.getElementById('answer-field');
 const answerPic = document.getElementById('answer-pic');
 const currentWordField = document.getElementById('current-word-field');
+const hintField = document.getElementById('hint-field');
 const numGuessesField = document.getElementById('num-guesses-field');
 const usedLettersField = document.getElementById('used-letters-field');
 const winCounterField = document.getElementById('win-counter-field');
 
 // Section 1: Initialize Document //
-current = answers[r(0, answers.length)];
-answer = current.name;
-hint = current.hint;
-maskAnswer();
-winCounter = 0;
-numGuesses = 15;
-remaining = answer.length;
-usedLetters = [];
-usedCorrect = [];
-numGuessesField.innerHTML = numGuesses;
-winCounterField.innerHTML = winCounter;
-usedLettersField.innerHTML = usedLetters;
+initialSet();
+
+// Log answer (for testing/ demonstration)
+console.log(answer);
 
 // Section 2: Functions //
+
+// Main Functions
 // Listen for Keyboard events
 window.addEventListener('keydown', function(e) {
-    const guess = e.key;
+    // Limit actions to letters of the alphabet
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const guess = e.key;
 
-    if (answer.includes(guess)) {
-        for (let i = 0; i < answer.length; i++) {
-            if (answer[i] === guess) {
-                document.getElementById('current').children[i].innerHTML = guess;
-                usedCorrect.push(guess);
-
-                // if (condition) {
-                //     remaining--;
-                // }
-
-                // console.log(answer.split(guess).length - 1);
-                // console.log(getFrequency(answer));
-                // console.log(usedCorrect);
-
-                console.clear;
-                console.log(guess);
-                console.log(countLetters(usedCorrect));
-
-                if (remaining === 0) {
-                    victoryScreen();
-                    answerPic.setAttribute('src', `/assets/img/${answer}.jpg`);
+        // If the guess is correct...
+        if (answer.includes(guess)) {
+            // Reveals correct letter(s)
+            for (let i = 0; i < answer.length; i++) {
+                // Update page
+                if (answer[i] === guess) {
+                    document.getElementById('current').children[i].innerHTML = guess;
                 }
             }
-        }
-    } else {
-        if (!usedLetters.includes(guess)) {
-            usedLetters.push(guess);
-            usedLettersField.innerHTML = usedLetters;
 
-            if (numGuesses > 1) {
-                numGuesses--;
-                numGuessesField.innerHTML = numGuesses;
-            } else {
-                numGuessesField.innerHTML = 'You Lose! Very Sad!';
+            // Stores correct guesses, and decriments remaining letters by the number of occurences
+            // (only first time per letter)
+            if (!usedCorrect.includes(guess)) {
+                usedCorrect.push(guess);
+                remaining -= answer.split(guess).length - 1;
+            }
+
+            // Reveals picture, hint, and name when all letters are guessed
+            if (remaining === 0) {
+                victoryScreen();
+            }
+
+            // If the guess is incorrect...
+        } else {
+            if (!usedLetters.includes(guess)) {
+                // Update page
+                usedLetters.push(guess);
+                usedLettersField.innerHTML = usedLetters;
+
+                // Decriment remaining guesses
+                if (numGuesses > 1) {
+                    numGuesses--;
+                    numGuessesField.innerHTML = numGuesses;
+
+                    // If user is out of guesses end the game
+                } else {
+                    loserScreen();
+                }
             }
         }
     }
 });
 
+// Initialize game
+function initialSet() {
+    // Initialize values
+    winCounter = 0;
+    current = answers[r(0, answers.length)];
+    answer = current.name;
+    hint = current.hint;
+    remaining = answer.length;
+    numGuesses = Math.round(remaining * 1.5); // Gives more guesses for long words, and fewer guesses for short words
+    usedLetters = [];
+    usedCorrect = [];
+
+    // Hide aanswer
+    maskAnswer();
+
+    // Set HTML
+    winCounterField.innerHTML = winCounter;
+    numGuessesField.innerHTML = numGuesses;
+    usedLettersField.innerHTML = usedLetters;
+}
+
+// On-Click Functions
+// Shows the hint
+function showHint() {
+    hintField.innerHTML = hint;
+}
+
+function reset() {
+    // Reset values (not win counter)
+    usedLetters = [];
+    usedCorrect = [];
+    currentWordField.innerHTML = '';
+    hintField.innerHTML = '';
+    answerField.innerHTML = 'Next Question!';
+    current = answers[r(0, answers.length)];
+    answer = current.name;
+    hint = current.hint;
+    remaining = answer.length;
+    numGuesses = Math.round(remaining * 1.5);
+    answerPic.removeAttribute('src');
+
+    // Hide new answer
+    maskAnswer();
+
+    // Reset HTML
+    numGuessesField.innerHTML = numGuesses;
+    usedLettersField.innerHTML = usedLetters;
+}
+
+// Background Functions
 // Create Masked Answer
 function maskAnswer() {
     const maskedAnswer = document.createElement('ul');
@@ -123,8 +174,21 @@ function maskAnswer() {
     }
 }
 
+// Displays information on victory
 function victoryScreen() {
+    winCounter++;
+    winCounterField.innerHTML = winCounter;
     answerField.innerHTML = answer;
+    hintField.innerHTML = hint;
+    answerPic.setAttribute('src', `/assets/img/${answer}.jpg`);
+}
+
+// Displays information on loss
+function loserScreen() {
+    numGuessesField.innerHTML = 'You Lose! Very Sad!';
+    answerField.innerHTML = answer;
+    hintField.innerHTML = hint;
+    answerPic.setAttribute('src', `/assets/img/${answer}.jpg`);
 }
 
 // Generates Random Number
@@ -134,18 +198,3 @@ function r(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-// Counts Frequency of a letter in an array
-function countLetters(arr) {
-    let counts = {};
-
-    for (let i = 0; i < arr.length; i++) {
-        let letter = arr[i];
-        counts[letter] = counts[letter] ? counts[letter] + 1 : 1;
-    }
-
-    return counts;
-}
-
-console.log(answer);
-console.log(hint);
